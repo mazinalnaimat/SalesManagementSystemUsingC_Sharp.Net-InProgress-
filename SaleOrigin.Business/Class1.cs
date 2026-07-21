676e0520d662b4092d571ba0678b06dc397b33cc
Mazin Al-Naimat <mazinalnaimat@gmail.com>
2026-07-21 13:45:51 +0300

    initiate project files

---
+++ SaleOrigin.Business/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaleOrigin.Business
+{
+    public class Class1
+    {
+    }
+}
